3168f547c4a375e4c07231c1824191b5a810f21e
Ankit Soni <ankit.soni05@gmail.com>
2023-01-21 19:40:16 +0530

    test

---
+++ asApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
